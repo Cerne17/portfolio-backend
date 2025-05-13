18010078a55d1a8124a15c3d18c76115cc435d24
Cerne <miguelbadanycerne.20231@poli.ufrj.br>
2025-05-13 19:26:33 -0300

    feat: first module - post

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PostsModule } from './posts/posts.module';
+import { PostsService } from './posts.service';
+import { PostsController } from './posts.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db.sqlite',
+      // For PostgreSQL, you'd add host, port, username, password:
+      // host: process.env.DB_HOST || 'localhost',
+      // port: parseInt(process.env.DB_PORT, 10) || 5432,
+      // username: process.env.DB_USERNAME || 'youruser',
+      // password: process.env.DB_PASSWORD || 'yourpassword',
+      // database: process.env.DB_NAME || 'portfolio_db'
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true, // Do not use in production
+    }),
+    PostsModule,
+  ],
+  controllers: [AppController, PostsController],
+  providers: [AppService, PostsService],
 })
 export class AppModule {}
