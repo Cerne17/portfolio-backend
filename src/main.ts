f2d9216d7a2dfb93bc07007a08034c622395694f
Cerne <miguelbadanycerne.20231@poli.ufrj.br>
2025-05-13 20:35:44 -0300

    feat: adds validation pipeline

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // Strips data not in dto
+      forbidNonWhitelisted: true, // Throws errors if extra data is sent
+      transform: true, // Automatically transforms payload data into dtos
+    }),
+  );
+  app.enableCors(); // Enables CORS for Frontend access
+  await app.listen(process.env.PORT ?? 3001); // Using port 3001 since React uses 3000/5173 as default
 }
 bootstrap();
